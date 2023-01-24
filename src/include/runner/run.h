2808294ad2d8bf28ae00b7e31405bb65c0e924a2
xUser5000 <abdallahar1974@gmail.com>
2023-01-25 01:47:38 +0200

    feat(runner): add methods to run command line tasks

---
+++ src/include/runner/run.h
@@ -0,0 +1,15 @@
+//
+// Created by xuser5000 on 25/01/23.
+//
+
+#ifndef BAKE_RUN_H
+#define BAKE_RUN_H
+
+#include <vector>
+#include <string>
+
+int run(std::string &command);
+
+void run(std::vector<std::string> &commands);
+
+#endif //BAKE_RUN_H
