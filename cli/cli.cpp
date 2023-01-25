db2c7afbf902c795fbc9a3e89529048ce1613989
xUser5000 <abdallahar1974@gmail.com>
2023-01-25 15:09:22 +0200

    feat(cli): implement basic CLI

--- cli/cli.cpp
+++ cli/cli.cpp
@@ -1,9 +1,17 @@
-#include "common/BakeInstance.h"
+#include <iostream>
+#include <fstream>
 
-int main() {
+#include "common/bake.h"
 
-    auto instance = new BakeInstance();
-    instance->bake();
+#define BAKEFILE_NAME "Bakefile"
 
+int main(int argc, char* argv[]) {
+    std::ifstream in(BAKEFILE_NAME);
+    if (!in.is_open()) {
+        std::cout << "Could not open Bakefile!\n";
+        return 1;
+    }
+    bake(in);
+    in.close();
     return 0;
 }
