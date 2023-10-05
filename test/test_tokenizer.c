69a3564936b5b6d19b5a82b6e2bf647f68d8f7c6
xUser5000 <abdllahar1974@gmail.com>
2023-10-05 22:01:09 +0300

    test: tokenizer

--- test/test_tokenizer.c
+++ test/test_tokenizer.c
@@ -1,10 +1,49 @@
 #include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
 
 #include "utest.h"
 #include "tokenizer.h"
 
 UTEST_MAIN();
 
-UTEST(tokenizer, hello) {
-    ASSERT_TRUE(1 == 1);
+#define ASSERT_TOKEN_EQ(tokenizer, token_content, token_type) { \
+    token_t *token = tokenizer_read_token(tokenizer); \
+    ASSERT_STREQ(token->content, token_content); \
+    ASSERT_EQ(token->type, token_type); }
+
+#define ASSERT_TOKEN_NULL(tokenizer) \
+    ASSERT_EQ(tokenizer_read_token(tokenizer), NULL)
+
+
+UTEST(Tokenizer, BasicScriptWithoutWhiteSpaces) {
+    char *test_string = "target:dependency\n\t\"recipe\"";
+    FILE *ss = fmemopen(test_string, strlen(test_string), "r");
+    tokenizer_t *tokenizer = tokenizer_init(ss);
+    
+    ASSERT_TOKEN_EQ(tokenizer, "target", IDENTIFIER);
+    ASSERT_TOKEN_EQ(tokenizer, ":", COLON);
+    ASSERT_TOKEN_EQ(tokenizer, "dependency", IDENTIFIER);
+    ASSERT_TOKEN_EQ(tokenizer, "\n", ENDLINE);
+    ASSERT_TOKEN_EQ(tokenizer, "\t", TAB);
+    ASSERT_TOKEN_EQ(tokenizer, "\"recipe\"", COMMAND);
+    ASSERT_TOKEN_NULL(tokenizer);
+}
+
+
+
+UTEST(Tokenizer, BasicScriptWithWhiteSpaces) {
+    char *test_string = "target  :dependency     \n\t\"recipe\"";
+    FILE *ss = fmemopen(test_string, strlen(test_string), "r");
+    tokenizer_t *tokenizer = tokenizer_init(ss);
+
+    ASSERT_TOKEN_EQ(tokenizer, "target", IDENTIFIER);
+    ASSERT_TOKEN_EQ(tokenizer, "  ", WHITE_SPACE);
+    ASSERT_TOKEN_EQ(tokenizer, ":", COLON);
+    ASSERT_TOKEN_EQ(tokenizer, "dependency", IDENTIFIER);
+    ASSERT_TOKEN_EQ(tokenizer, "     ", WHITE_SPACE);
+    ASSERT_TOKEN_EQ(tokenizer, "\n", ENDLINE);
+    ASSERT_TOKEN_EQ(tokenizer, "\t", TAB);
+    ASSERT_TOKEN_EQ(tokenizer, "\"recipe\"", COMMAND);
+    ASSERT_TOKEN_NULL(tokenizer);
 }
